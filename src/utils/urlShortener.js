d7023aa1301cb748b7dad6acdb1284527efb4b14
Percy <achataipepercy2000@gmail.com>
2026-01-18 18:19:07 -0500

    Revert: Disabled URL shortening as per user request

--- src/utils/urlShortener.js
+++ src/utils/urlShortener.js
@@ -6,16 +6,6 @@ import { SHORTENER_API, LOCAL_DEV_IP } from '../config/appConfig';
  * @returns {string} - The shortened URL or original if all fail
  */
 export const shortenUrl = async (longUrl) => {
-    try {
-        // Use TinyURL API directly (free and no token needed for simple shortening)
-        const response = await fetch(`https://tinyurl.com/api-create.php?url=${encodeURIComponent(longUrl)}`);
-        if (response.ok) {
-            const shortUrl = await response.text();
-            return shortUrl;
-        }
-        return longUrl;
-    } catch (error) {
-        console.error('Shortener error:', error);
-        return longUrl;
-    }
+    // Return original URL as requested by user
+    return longUrl;
 };
